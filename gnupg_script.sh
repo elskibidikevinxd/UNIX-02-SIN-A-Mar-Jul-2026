c78399dfd95951dc61f4dcbd32cec805c528aa6f
elskibidikevinxd <kevinalmache7@gmail.com>
2026-03-20 18:04:19 +0000

    llaves xd

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -1,3 +1,4 @@
 uname -a /usr/bin/gpg #daba informacion del ordenador
 wich gpg # ubicacion del binario de gpg
-gpg --version
+gpg --version #muestra la version
+gpg --full-generate-key #para generar un par de llaves
