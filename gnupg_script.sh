373e3eeeb0fa44ee1a356b2d5073a59501ce1152
elskibidikevinxd <kevinalmache7@gmail.com>
2026-03-20 18:10:58 +0000

    exportacionn llaves

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -1,4 +1,6 @@
 uname -a /usr/bin/gpg #daba informacion del ordenador
 wich gpg # ubicacion del binario de gpg
 gpg --version #muestra la version
-gpg --full-generate-key #para generar un par de llaves
+gpg --full-generate-key #para generar un par de llaves  
+gpg --list-keys
+gpg --armor --export jonathan.e.tito.0@gmail.com > mi_llave_publica.asc
