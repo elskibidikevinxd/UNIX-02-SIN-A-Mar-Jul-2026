23f34189d919d1e25452276cb4a7089d3eadfaeb
elskibidikevinxd <kevinalmache7@gmail.com>
2026-03-26 12:37:24 +0000

    secret key

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -2,5 +2,6 @@ uname -a /usr/bin/gpg #daba informacion del ordenador
 wich gpg # ubicacion del binario de gpg
 gpg --version #muestra la version
 gpg --full-generate-key #para generar un par de llaves  
-gpg --list-keys
-gpg --armor --export jonathan.e.tito.0@gmail.com > mi_llave_publica.asc
+gpg --list-keys #lista las llaves
+gpg --armor --export kevinalmache7@gmail.com > mi_llave_publica.asc #exportar a archivo y formato legible
+gpg --list-secret-keys --keyid-format=long #para listar llaves privadas
