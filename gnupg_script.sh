8008f12bfbf867412674e876581a6e3f0deb20ea
elskibidikevinxd <kevinalmache7@gmail.com>
2026-03-26 13:06:18 +0000

    llave amigo

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -5,3 +5,6 @@ gpg --full-generate-key #para generar un par de llaves
 gpg --list-keys #lista las llaves
 gpg --armor --export kevinalmache7@gmail.com > mi_llave_publica.asc #exportar a archivo y formato legible
 gpg --list-secret-keys --keyid-format=long #para listar llaves privadas
+gpg --armor --export-secret-keys XXXXXXXXXXX #para exportar mis llaves privadas, debi a un problema yo use, gpg --pinentry-mode loopback --export-secret-keys --armor XXXXXXXXXXXXXXX (valor del hash)
+gpg -- mport llave:punlica.asc #lo usamos para importar la llave de nuestro amigo
+gpg --list-keys #verificamos que tengamos la llave de nuestro amigo
