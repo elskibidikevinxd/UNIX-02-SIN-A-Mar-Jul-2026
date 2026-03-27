2c2fbaf0f6e8f6ad1d4ee20c3fab8f9ea3cf23dd
elskibidikevinxd <kevinalmache7@gmail.com>
2026-03-27 17:11:06 +0000

    comprobacion de q se puede ejucutar saludobin desde cualquier carpeta

--- filesystem_scrpt.sh
+++ filesystem_scrpt.sh
@@ -2,4 +2,16 @@ cd /dev #change directory to /dev directory
 ls #list files and directories on the /dev directory
 cd /proc #
 ls #lost files and directories on the /proc
-cat cpuinfo #it helps to search the info of the cpu
+cat cpuinfo #it helps to search the info of the cpu 
+cd /workspaces/UNIX-02-SIN-A-Mar-Jul-2026/ #para movernos al main
+ls #buscamos lo que tenemos en el main
+gcc saludo.c  -o saludobin
+sudo mv saludobin /bin/
+saludobin
+cd /etc
+saludobin
+cd /dev
+saludobin
+cd /proc
+saludobin
+cd /workspaces/UNIX-02-SIN-A-Mar-Jul-2026/
