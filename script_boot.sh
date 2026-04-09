9915860dcedf84c1dd159c7503c3329f4d937e88
elskibidikevinxd <kevinalmache7@gmail.com>
2026-04-09 12:24:00 +0000

    first-commit

---
+++ script_boot.sh
@@ -0,0 +1,3 @@
+mkdir proyecto #it create an directory named project
+cd proyecto/ #it changes the directory to project using a relative way
+cd /workspaces/UNIX-02-SIN-A-Mar-Jul-2026/proyecto #it changes the directory to project using a absolute way
