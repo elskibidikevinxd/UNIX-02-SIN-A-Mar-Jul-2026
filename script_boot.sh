0388dd3a3482cf819be1421c0ff3dbc127d9215d
elskibidikevinxd <kevinalmache7@gmail.com>
2026-04-09 12:39:25 +0000

    second-commit

--- script_boot.sh
+++ script_boot.sh
@@ -1,3 +1,10 @@
 mkdir proyecto #it create an directory named project
-cd proyecto/ #it changes the directory to project using a relative way
-cd /workspaces/UNIX-02-SIN-A-Mar-Jul-2026/proyecto #it changes the directory to project using a absolute way
+cd proyecto/ #it changes the directory to project using a relative path
+cd /workspaces/UNIX-02-SIN-A-Mar-Jul-2026/proyecto #it changes the directory to project using a absolute path
+cd #it changes the directory to home
+pwd #it shows the directory where we are
+cd ~ #it changes the directory to home using a relative path
+cd /home/codespace/ #changes the directory to home using a absolute path
+echo $HOME #it shows the absolute path of home
+echo $BASH #it shows the absolute path of bash
+echo $PATH #it shows all paths the system can use
