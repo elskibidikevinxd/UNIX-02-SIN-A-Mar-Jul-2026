c706b33758d4e1828330162c853bcf2cfe3f37a8
elskibidikevinxd <kevinalmache7@gmail.com>
2026-04-09 13:07:00 +0000

    third-commit

--- script_boot.sh
+++ script_boot.sh
@@ -8,3 +8,11 @@ cd /home/codespace/ #changes the directory to home using a absolute path
 echo $HOME #it shows the absolute path of home
 echo $BASH #it shows the absolute path of bash
 echo $PATH #it shows all paths the system can use
+ls -lai #it shows the secret archives, the inodes and the size, modification date
+ls -l -a -i #it's the same of ls -lai, in this case it's just longer, -l You can see the permissions, the owner, the size, and the modification date, -a
+#Shows all files, including hidden ones (those that begin with .) and -i shows the inode (the unique id)
+man ls #it shows the manual
+ls --all #it shows all in the actual directory and the difference with ls -a it's the ls --all is the complete comand
+ls -a #it shows the same that ls -all, ls -a is only a short way to write the command
+. #it's the actual directory
+.. #It's the directory one folder up (parent directory)
