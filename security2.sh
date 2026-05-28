eba726428f296e6c2bb9720b2ca7a0dfc954ba9d
elskibidikevinxd <kevinalmache7@gmail.com>
2026-05-28 13:36:06 +0000

    commit 7

--- security2.sh
+++ security2.sh
@@ -19,3 +19,5 @@ exit # Exits the current sub-shell or group session.
 id -gn # Displays the name of your current primary group.
 echo "Grupo restaurado: $(id -gn)" # Prints the text "Grupo restaurado:" followed by your current primary group name.
 ls -la ~/antes_de_newgrp.txt ~/dentro_de_newgrp.txt # Lists both files at the same time to compare their sizes, permissions, and group ownerships.
+groupadd grupo_restringido # Creates a new system group named 'grupo_restringido'.
+gpasswd grupo_restringido # Administers the group configuration, usually prompted to set or modify the group password. the password is "12345678"
