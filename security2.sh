a243096e14614a42eda811264589b2ce0884add1
elskibidikevinxd <kevinalmache7@gmail.com>
2026-05-28 13:14:21 +0000

    tercer commit (pd el primer commit no hay pq me olvide crear el security y no revise que no se habia guardado el commit)

--- security2.sh
+++ security2.sh
@@ -7,3 +7,7 @@ ls -la ~/antes_de_newgrp.txt # Lists the newly created file with detailed inform
 newgrp nombre_grupo # Log in to a new group (changes your current primary group).
 touch ~/grupo_heredado.txt # Creates an empty file named 'grupo_heredado.txt' in your home directory (or updates its timestamp if it already exists).
 ls -la ~/grupo_heredado.txt # Lists the newly created file with detailed information (permissions, owner, group, size, and date), including hidden attributes.
+groupadd desarrolladores
+su - root -g desarrolladores
+id -gn
+echo "Nuevo grupo activo: $(id -gn)"
