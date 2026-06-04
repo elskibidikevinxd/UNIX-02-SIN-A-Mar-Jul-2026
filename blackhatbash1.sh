feaad8c70494875b237f53d48e3bbcb1d35027b7
elskibidikevinxd <kevinalmache7@gmail.com>
2026-06-04 12:42:39 +0000

    commit 1

---
+++ blackhatbash1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+bash --version      # Displays the installed version and details of the Bash shell
+echo ${RANDOM}      # Prints a pseudo-random integer between 0 and 32767
+echo ${SHELL}       # Prints the path to the current user's default login shell
+env                 # Lists all current environment variables exported in the session
+echo ${UID}         # Prints the User ID (numeric identifier) of the current logged-in user
+echo ${OSTYPE}      # Prints the operating system type/environment (e.g., linux-gnu)
