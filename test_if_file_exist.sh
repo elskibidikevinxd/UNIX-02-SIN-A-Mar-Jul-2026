798c81398ce76ffad6f3a687d9886560a7190920
elskibidikevinxd <kevinalmache7@gmail.com>
2026-06-11 12:59:03 +0000

    commit 12

--- test_if_file_exist.sh
+++ test_if_file_exist.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-FILENAME="flow_control_with_if.txt"
+FILENAME="flow_control_with_if1.txt"
 
 if [[ ! -f "${FILENAME}" ]]; then
-    echo "${FILENAME} already exists."
-    exit 1
-else 
-touch "${FILENAME}"
+    touch "${FILENAME}"
 fi
