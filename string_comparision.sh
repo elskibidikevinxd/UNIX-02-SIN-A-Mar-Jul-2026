7b56db44b6af2602a1d66d56e0943bd9d63a2614
elskibidikevinxd <kevinalmache7@gmail.com>
2026-06-11 13:05:21 +0000

    commit 14

--- string_comparision.sh
+++ string_comparision.sh
@@ -2,8 +2,8 @@
 VARIABLE_ONE="10"
 VARIABLE_TWO="nostarch"
 
-if [[ "${VARIABLE_ONE}" == "${VARIABLE_TWO}" ]]; then
-    echo "they are equal!"
+if [[ "${VARIABLE_ONE}" -gt "${VARIABLE_TWO}" ]]; then
+    echo "${VARIABLE_ONE} is greater than ${VARIABLE_TWO}."
 else
-    echo "they are not equal!"
+    echo "${VARIABLE_ONE} is less than ${VARIABLE_TWO}."
 fi
