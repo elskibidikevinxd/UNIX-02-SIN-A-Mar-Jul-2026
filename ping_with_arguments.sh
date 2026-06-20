ca1c697c9a959570a2bd4c0bb2d1fba57840ccab
elskibidikevinxd <kevinalmache7@gmail.com>
2026-06-20 04:15:39 +0000

    commit 39

---
+++ ping_with_arguments.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# This script will ping any address provided as an argument.
+SCRIPT_NAME="${0}"
+TARGET="${1}"
+echo "Running the script ${SCRIPT_NAME}..."
+echo "Pinging the target: ${TARGET}..."
+ping -c 5 "${TARGET}"
+echo "The arguments are: $*"
+echo "The total number of arguments is: $#"
+for args in "$@"; do
+    echo "${args}"
+done
+echo "What is your first name?"
+read -r firstname
+echo "What is your last name?"
+read -r lastname
+echo "Your first name is ${firstname} and your last name is ${lastname}"
